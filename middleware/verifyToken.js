c881b9d4823fe6305e1bd2b807ee2980299bc177
RufatBayramov22 <ibrahimovrufka@icloud.com>
2024-06-22 20:36:48 +0400

    joj

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,15 +1,17 @@
+import jwt from 'jsonwebtoken';
+
 export const verifyToken = (req, res, next) => {
-  const token = req.cookies.token;
+    const token = req.cookies.token || req.headers['authorization']?.split(' ')[1];
 
-  if (!token) {
-      return res.status(401).json({ message: "Unauthorized" });
-  }
+    if (!token) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
 
-  try {
-      const verified = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = verified;
-      next();
-  } catch (err) {
-      res.status(400).json({ message: "Invalid Token" });
-  }
+    try {
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = verified;
+        next();
+    } catch (err) {
+        res.status(400).json({ message: "Invalid Token" });
+    }
 };
