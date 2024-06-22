d1b515b7a9bddf8700420b8daa48d50601d10382
RufatBayramov22 <ibrahimovrufka@icloud.com>
2024-06-22 15:31:25 +0400

    new save

--- app.js
+++ app.js
@@ -22,7 +22,12 @@ app.options('*', (req, res) => {
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.sendStatus(200);
   });
-
+  
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', ['http://localhost:3000', 'http://tapal.az']); // Add your domain
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 
 app.use(express.json());
 app.use(cookieParser()); 
