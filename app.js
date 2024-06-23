6512167b25e3f28f3b9419b261a55d125b759280
RufatBayramov22 <ibrahimovrufka@icloud.com>
2024-06-23 13:57:17 +0400

    sss

--- app.js
+++ app.js
@@ -32,9 +32,9 @@ app.use(cookieParser());
 
 app.use(express.static('public'));
 
-app.use("/api/auth", authRoute);
-app.use("/api/posts", postRoute);
-app.use("/api/users", userRoute);
+app.use("/auth", authRoute);
+app.use("/posts", postRoute);
+app.use("/users", userRoute);
 
 app.use((req, res, next) => {
     res.status(404).send('Not Found');
