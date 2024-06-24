3ae000011dba9338ffaf73bcf2ef97bc2237afbb
RufatBayramov22 <ibrahimovrufka@icloud.com>
2024-06-24 21:09:13 +0400

    post

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -25,6 +25,7 @@ export const getPosts = async (req, res) => {
 
 export const getPost = async (req, res) => {
   const id = req.params.id;
+
   try {
     const post = await prisma.post.findUnique({
       where: { id },
@@ -38,35 +39,37 @@ export const getPost = async (req, res) => {
         },
       },
     });
+
+    if (!post) {
+      return res.status(404).json({ message: "Post bulunamadı" });
+    }
+
     const token = req.cookies?.token;
 
     if (token) {
-      jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
-        if (err) {
-          return res.status(401).json({ message: "Unauthorized" });
-        }
-        try {
-          const saved = await prisma.savedPost.findUnique({
-            where: {
-              userId_postId: {
-                postId: id,
-                userId: payload.id,
-              },
+      try {
+        const payload = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+        const saved = await prisma.savedPost.findUnique({
+          where: {
+            userId_postId: {
+              postId: id,
+              userId: payload.id,
             },
-          });
-          return res.status(200).json({ ...post, isSaved: saved ? true : false });
-        } catch (error) {
-          console.error(error);
-          return res.status(500).json({ message: "Internal Server Error" });
-        }
-      });
+          },
+        });
+
+        return res.status(200).json({ ...post, isSaved: !!saved });
+      } catch (err) {
+        console.error("JWT doğrulama hatası:", err);
+        return res.status(401).json({ message: "Yetkisiz" });
+      }
     } else {
       return res.status(200).json({ ...post, isSaved: false });
     }
-    
   } catch (err) {
-    console.log(err);
-    res.status(500).json({ message: "Failed to get Post" });
+    console.error("Veritabanı hatası:", err);
+    return res.status(500).json({ message: "Sunucu Hatası" });
   }
 };
 
